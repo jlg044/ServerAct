5663d29a1ae93b3b3099e132eaaa88900aabb159
Mario-Petersen <m318365@alunos.ipb.pt>
2024-12-03 14:44:40 +0100

    SubirArchivos cambiado

--- subirArchivos.py
+++ subirArchivos.py
@@ -1,31 +1,36 @@
 import os
 import requests
-
-# Directorio de actualizaciones
-UPDATE_DIR = './static'
+import argparse
 
 # Lista para almacenar las actualizaciones
-updates = []
+url = "http://127.0.0.1:8000/updates"
 
 # Especificar los tags que deseas buscar
 def deftags(nomb):
-    global updates  # Usar la lista global de updates
+
+    updates = []  # Usar la lista global de updates
+    
     for tag in nomb:
-        tag_dir = os.path.join(UPDATE_DIR, tag)
+        tag_dir = os.path.join(path, tag)
         if os.path.isdir(tag_dir):  # Verificar que el subdirectorio exista
             for filename in os.listdir(tag_dir):  # Recorrer archivos en el subdirectorio
                 # Añadir el tag y el nombre completo del archivo
                 updates.append({"tag": tag, "version": filename})
+        else: print(f"Directorio {tag_dir} no existe")
+    return updates
+
+
 
 # Función para subir archivos al servidor
 def upload_file(tag, file_name):
-    url = f"http://127.0.0.1:8000/updates/{tag}/{file_name}"
+    urlFile = os.path.join(url, tag, file_name).replace('\\','/')
     try:
         # Construir la ruta completa al archivo
-        file_path = os.path.join(UPDATE_DIR, tag, file_name)
+        file_path = os.path.join(path, tag, file_name).replace('\\','/')
+
         with open(file_path, 'rb') as file:
             files = {'file': file}
-            response = requests.put(url, files=files)
+            response = requests.put(urlFile, files=files)
 
             # Verificar si la respuesta es un JSON antes de intentar decodificarla
             try:
@@ -44,12 +49,28 @@ def upload_file(tag, file_name):
     except PermissionError:
         print(f"No se pudo acceder al archivo {file_name} debido a permisos insuficientes.")
 
-# Inicializar tags y llenar updates
-nomb = ["Vega22", "Vega11"]
-deftags(nomb)
+# Main
+if __name__ == '__main__':
+    # Setup
+    parser = argparse.ArgumentParser(description='Editar manualmente los sectores sobre un set de imágenes')
+    parser.add_argument('set_path', type=str, help='Ruta al set de imágenes')
+    args = parser.parse_args()
+    path = args.set_path.replace('\\', '/')
+    if path[-1] != '/':
+        path += '/'
+    if not os.path.exists(path):
+        raise Exception('No existe el directorio')
+    files = [file for file in os.listdir(path)]
+    if not files:
+        raise Exception('No hay archivos en el directorio')
+    
+    # Inicializar tags y llenar updates
+    nomb = ["Vega22", "Vega11"]
+    updates = deftags(nomb)
 
-# Subir todos los archivos de la lista de actualizaciones
-for update in updates:
-    tag = update["tag"]
-    file_name = update["version"]
-    upload_file(tag, file_name)
+    # Subir todos los archivos de la lista de actualizaciones
+    for update in updates:
+        tag = update["tag"]
+        file_name = update["version"]
+        upload_file(tag, file_name)
+    
