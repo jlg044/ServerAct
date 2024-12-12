a897bb099b773d7775bd4d517742405b664d92d1
jlg044 <selu9029@gmail.com>
2024-12-12 12:48:34 +0100

    server.py arreglado

--- Server/server.py
+++ Server/server.py
@@ -4,6 +4,7 @@ from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 import os
 import Updater.updateConfig as up
+import Updater.database as db
 
 #Main
 
@@ -29,7 +30,9 @@ if __name__ == "__main__":
 @app.get("/updates/{tag}")
 async def list_updates(tag: str):
     """Devuelve todas las versiones disponibles para un tag."""
-    versiones = listVersions(tag)
+    versiones = []
+    versiones = db.listVersions(tag)
+    return versiones
     
     #tag_dir = os.path.join(UPDATE_DIR, tag)
     #if os.path.isdir(tag_dir):
@@ -124,13 +127,13 @@ def iteracionArchivos(dir,tag,updates):
 
 # Obtener Tags de la base de datos
 def obtTags():
-    tags = up.obtenerTags()
+    tags = db.obtenerTags()
     return tags
 
 # Subir Tags a la base de datos
 def subTags(tags):
-    up.subirTags(tags)
+    db.subirTags(tags)
 
 def compAct(tag, versAct):
-    cambios = up.comprobarActualizacion(tag, versAct)    
+    cambios = db.comprobarActualizacion(tag, versAct)    
     return cambios
